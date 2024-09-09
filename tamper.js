55daee7c9cad1cabc2fb818f9ad58fd07f12e1c0
TyrWark <167569189+TyrWark@users.noreply.github.com>
2024-09-09 14:23:11 -0500

    Create tamper.js

---
+++ tamper.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Project Mimir
+// @namespace    http://tampermonkey.net/
+// @version      2024-06-17
+// @description  Ask an AI to generate a report for you!
+// @author       Ty Wark
+// @match        https://lightspeedanalytics.net/app/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @require      https://raw.githubusercontent.com/TyrWark/ProjectMimir-InProduct/main/Include.js
+// @run-at document-idle
+// ==/UserScript==
+
+
+console.log("Project Mimir")
+var apikey = "REPLACEME"
